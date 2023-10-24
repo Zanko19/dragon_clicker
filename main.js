fc412e1da96e4d012bc2ebe2ba8496f9102bce4e
Guillaume <guizanko@hotmail.fr>
2023-10-24 10:38:36 +0200

    click

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
 import "./style.scss";
 import "./script.js";
+// Attendez que le DOM soit chargé
+document.addEventListener("DOMContentLoaded", function () {
+    // Sélectionnez l'élément d'œuf
+    const eggElement = document.getElementById("egg-container");
+  
+    // Initialisez le compteur d'œufs
+    let eggs = 0;
+  
+    // Fonction pour incrémenter le compteur d'œufs
+    function incrementEggCount() {
+      eggs++;
+      updateEggCounter();
+    }
+  
+    // Fonction pour mettre à jour le compteur d'œufs sur la page
+    function updateEggCounter() {
+      const eggCounterElement = document.getElementById("egg-count");
+      eggCounterElement.textContent = `Dragon Egg : ${eggs}`;
+    }
+  
+    // Écoutez le clic sur l'œuf
+    eggElement.addEventListener("click", incrementEggCount);
+  });
+  
